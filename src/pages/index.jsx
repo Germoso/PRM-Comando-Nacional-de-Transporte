7e0a45db4efbfbad4b522a712f41a39d55e25c3c
Alexander Germoso <mailonywilo@gmail.com>
2023-04-12 10:18:01 -0400

    update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,9 @@
 import React from "react"
 import { Card, Container, Grid, Navbar, Row, Text } from "@nextui-org/react"
 import Form from "@/components/Form/Form"
+import Image from "next/image"
+// import Four from "@/components/icons/Four"
+import four from "../../public/four.png"
 
 const Index = () => {
     return (
@@ -13,6 +16,12 @@ const Index = () => {
                             <Text size={"$2xl"} h1 css={{ marginTop: "$10", textAlign: "center" }}>
                                 EQUIPO NACIONAL DE TRANSPORTE
                             </Text>
+                            <Image
+                                src={four}
+                                alt="4+"
+                                width={100}
+                                style={{ position: "absolute", right: 20, top: -5 }}
+                            />
                             <Text h4 css={{ textAlign: "center" }}>
                                 Registro Nacional de militantes
                             </Text>
